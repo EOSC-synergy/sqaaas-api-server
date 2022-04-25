c101f2389454d1bcdf3b88c127c3a99eec57afe1
Pablo Orviz <orviz@ifca.unican.es>
2022-04-25 16:37:16 +0200

    Fix: content indentation

--- openapi_server/templates/commands_script.sh
+++ openapi_server/templates/commands_script.sh
@@ -1,20 +1,20 @@
 (
 {% if template.startswith("kubectl") %}
-    {%- if template in ["kubectl_config_files"] %}
-    {%- set k8s_config_files = template_kwargs.get("k8s_config_files", []) -%}
-    {%- set k8s_rollout_status_timeout = template_kwargs.get("k8s_rollout_status_timeout", "0") -%}
+{%- if template in ["kubectl_config_files"] %}
+{%- set k8s_config_files = template_kwargs.get("k8s_config_files", []) -%}
+{%- set k8s_rollout_status_timeout = template_kwargs.get("k8s_rollout_status_timeout", "0") -%}
 cat <<EOF >> {{ checkout_dir }}/kustomization.yaml
 resources:
 {%- for cfile in k8s_config_files %}
 - {{ checkout_dir }}/{{ cfile }}
 {%- endfor %}
 EOF
-    {%- endif %}
-    kubectl apply -k {{ checkout_dir }}
-    if ! kubectl rollout status -k {{ checkout_dir }} --timeout={{ k8s_rollout_status_timeout }}; then
-        kubectl rollout undo -k {{ checkout_dir }} --timeout={{ k8s_rollout_status_timeout }}
-        kubectl rollout status -k {{ checkout_dir }} --timeout={{ k8s_rollout_status_timeout }}
-    fi
+{%- endif %}
+kubectl apply -k {{ checkout_dir }}
+if ! kubectl rollout status -k {{ checkout_dir }} --timeout={{ k8s_rollout_status_timeout }}; then
+    kubectl rollout undo -k {{ checkout_dir }} --timeout={{ k8s_rollout_status_timeout }}
+    kubectl rollout status -k {{ checkout_dir }} --timeout={{ k8s_rollout_status_timeout }}
+fi
 {%- else %}
 cd {{ checkout_dir }} &&
     {%- for cmd in commands %}
