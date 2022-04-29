97c7f1c0af69d67776aaf454c2a64fb325571c3a
Pablo Orviz <orviz@ifca.unican.es>
2022-04-29 12:17:41 +0200

    Parametrize test data for test_get_registry_from_image

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,18 @@
+import pytest
+
 from openapi_server.controllers import utils
 
 
-def test_get_registry_from_image():
-    image_name = 'mydockerregistry.example.com:8080/eoscsynergy/sqaaas-api-server:2.1.0'
-    expected_result = 'mydockerregistry.example.com:8080'
-    result = utils.get_registry_from_image(image_name)
-    assert result == expected_result
+expected_registry_url = [
+    ('https://mydockerregistry.example.com:8080/eoscsynergy/sqaaas-api-server:2.1.0', 'https://mydockerregistry.example.com:8080'),
+    ('https://mydockerregistry.example.com/eoscsynergy/sqaaas-api-server:2.1.0', 'https://mydockerregistry.example.com'),
+    ('http://mydockerregistry.example.com:8080/eoscsynergy/sqaaas-api-server:2.1.0', 'http://mydockerregistry.example.com:8080'),
+    ('http://mydockerregistry.example.com/eoscsynergy/sqaaas-api-server:2.1.0', 'http://mydockerregistry.example.com'),
+    ('mydockerregistry.example.com:8080/eoscsynergy/sqaaas-api-server:2.1.0', 'mydockerregistry.example.com:8080'),
+    ('mydockerregistry.example.com/eoscsynergy/sqaaas-api-server:2.1.0', 'mydockerregistry.example.com')
+]
+
+@pytest.mark.parametrize('image_name,expected', expected_registry_url)
+def test_get_registry_from_image(image_name, expected):
+    registry_url = utils.get_registry_from_image(image_name)
+    assert registry_url == expected
