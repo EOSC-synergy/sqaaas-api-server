f56a64d81c6f14923dbf23f46609bd786fe9ddcf
Pablo Orviz <orviz@ifca.unican.es>
2022-05-03 09:10:45 +0000

    init_utils() method to instantiate Jenkins, Git and Badgr utils

--- openapi_server/controllers/default_controller.py
+++ openapi_server/controllers/default_controller.py
@@ -20,6 +20,7 @@ from deepdiff import DeepDiff
 import namegenerator
 
 from openapi_server import config
+from openapi_server import controllers
 from openapi_server.controllers import db
 from openapi_server.controllers.badgr import BadgrUtils
 from openapi_server.controllers.git import GitUtils
@@ -39,46 +40,13 @@ SUPPORTED_PLATFORMS = {
 REPOSITORY_BACKEND = config.get(
     'repository_backend'
 )
-TOKEN_GH_FILE = config.get_repo(
-    'token', fallback='/etc/sqaaas/.gh_token')
 GITHUB_ORG = config.get_repo('organization')
-
-TOKEN_JK_FILE = config.get_ci(
-    'token', fallback='/etc/sqaaas/.jk_token')
-JENKINS_URL = config.get_ci('url')
-JENKINS_USER = config.get_ci('user')
 JENKINS_GITHUB_ORG = config.get_ci('github_organization_name')
-
-TOKEN_BADGR_FILE = config.get_badge(
-    'token', fallback='/etc/sqaaas/.badgr_token')
-BADGR_URL = config.get_badge('url')
-BADGR_USER = config.get_badge('user')
-BADGR_ISSUER = config.get_badge('issuer')
 
 logger = logging.getLogger('sqaaas.api.controller')
 
 
-def init_utils():
-    global git_utils, gh_utils, jk_utils, badgr_utils
-
-    # Instance of code repo backend object
-    with open(TOKEN_GH_FILE,'r') as f:
-        token = f.read().strip()
-    logger.debug('Loading GitHub token from local filesystem')
-    gh_utils = GitHubUtils(token)
-    git_utils = GitUtils(token)
-
-    # Instance of CI system object
-    with open(TOKEN_JK_FILE,'r') as f:
-        jk_token = f.read().strip()
-    logger.debug('Loading Jenkins token from local filesystem')
-    jk_utils = JenkinsUtils(JENKINS_URL, JENKINS_USER, jk_token)
-
-    # Instance of Badge issuing service object
-    with open(TOKEN_BADGR_FILE,'r') as f:
-        badgr_token = f.read().strip()
-    logger.debug('Loading Badgr password from local filesystem')
-    badgr_utils = BadgrUtils(BADGR_URL, BADGR_USER, badgr_token, BADGR_ISSUER)
+git_utils, gh_utils, jk_utils, badgr_utils = controllers.init_utils()
 
 
 async def _add_pipeline_to_db(body, report_to_stdout=False):
