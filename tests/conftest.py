77ffa079dd44f4be9b1f7188fc4e211b2935a7f7
Pablo Orviz <orviz@ifca.unican.es>
2022-05-06 14:25:42 +0200

    Extend methods of MockGitHubUtils class

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,10 +15,23 @@ pytest_plugins = [
 ]
 
 
+class MockGitHubRepo(object):
+    @staticmethod
+    def default_branch():
+        return 'main'
+
 class MockGitHubUtils:
     @staticmethod
     def get_file(*args, **kwargs):
         return "file content"
+
+    @staticmethod
+    def get_repository(*args, **kwargs):
+        return MockGitHubRepo()
+
+    @staticmethod
+    def get_commit_url(*args, **kwargs):
+        return 'https://example.com/commit_url'
 
 
 @pytest.fixture
