c1f1368f5378dc478cd36356d7173dcbe39e40a9
Pablo Orviz <orviz@ifca.unican.es>
2022-05-09 11:47:36 +0200

    Test case for the <keepgoing> flag

--- tests/fixtures/db.py
+++ tests/fixtures/db.py
@@ -2,23 +2,26 @@ import pytest
 
 
 class MockDB:
-    entry = {
+    db = {
+        'dd7d8481-81a3-407f-95f0-a2f1cb382a4b': {
             'pipeline_repo': 'org/repo_name',
             'pipeline_repo_url': 'https://example.com/org/repo_name',
             'data': {
-                'config': {
-                    'data_json': {
-                        'config': {
-                            'project_repos': {},
+                'config': [
+                    {
+                        'data_json': {
+                            'config': {
+                                'project_repos': {},
+                            },
+                            'credentials': [],
+                            'environment': {},
+                            'sqa_criteria': {}
                         },
-                        'credentials': [],
-                        'environment': {},
-                        'sqa_criteria': {}
+                        'data_yml': '',
+                        'data_when': {},
+                        'file_name': '',
                     },
-                    'data_yml': '',
-                    'data_when': {},
-                    'file_name': '',
-                },
+                ],
                 'composer': {
                     'data_json': {},
                     'data_yml': '',
@@ -28,14 +31,15 @@ class MockDB:
                 'commands_scripts': []
             }
         }
+    }
 
     @staticmethod
     def load_content():
-        return {'dd7d8481-81a3-407f-95f0-a2f1cb382a4b': MockDB.entry}
+        return MockDB.db
 
     @staticmethod
-    def get_entry(cls, *args, **kwargs):
-        return MockDB.entry
+    def get_entry(*args, **kwargs):
+        return MockDB.db['dd7d8481-81a3-407f-95f0-a2f1cb382a4b']
 
     @staticmethod
     def update_jenkins(*args, **kwargs):
