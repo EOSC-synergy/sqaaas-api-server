cd15374286765f85ff2c6261260ef8fad9c48175
Pablo Orviz <orviz@ifca.unican.es>
2022-06-02 17:07:58 +0200

    Raise SQAaaSAPIException if deployment object does not contain required data

--- openapi_server/controllers/default_controller.py
+++ openapi_server/controllers/default_controller.py
@@ -139,12 +139,16 @@ async def _get_tooling_for_assessment(
         elif criterion_id in ['SvcQC.Dep']:
             if repo_deploy:
                 repo = repo_deploy
-            criterion_data_copy['tools'] = [deployment['deploy_tool']]
-            logger.debug((
-                'Restricting SvcQC.Dep tools to the one choosen by the '
-                'user: %s' % criterion_data_copy['tools']
-            ))
-            filter_tool_by_requirement_level = False
+            try:
+                criterion_data_copy['tools'] = [deployment['deploy_tool']]
+            except KeyError as e:
+                raise SQAaaSAPIException(422, str(e))
+            else:
+                logger.debug((
+                    'Restricting SvcQC.Dep tools to the one choosen by the '
+                    'user: %s' % criterion_data_copy['tools']
+                ))
+                filter_tool_by_requirement_level = False
         logger.info('Using repository <%s> for criterion <%s>' % (
             repo['repo'], criterion_id)
         )
