947270b738c48fae323f15d5e1f1c2a20be621f8
Pablo Orviz <orviz@ifca.unican.es>
2024-01-08 08:55:54 +0000

    Fix: correct return from decorated function

--- openapi_server/controllers/git.py
+++ openapi_server/controllers/git.py
@@ -256,7 +256,6 @@ class GitUtils(object):
                     # repo settings
                     kwargs['tag'] = source_repo_branch
                     kwargs['commit_id'] = repo.commit(source_repo_branch).hexsha
-
-            ret = f(*args, **kwargs)
+                ret = f(*args, **kwargs)
             return ret
         return decorated_function
