8a7bb0dd59facff6cab26799447f9b3feac4e41f
Pablo Orviz <orviz@ifca.unican.es>
2024-10-07 12:52:49 +0200

    Skeleton for custom assessments

--- openapi_server/controllers/default_controller.py
+++ openapi_server/controllers/default_controller.py
@@ -21,14 +21,12 @@ from urllib import parse as urllib_parse
 from zipfile import ZipFile, ZipInfo
 
 import namegenerator
+import openapi_server
 import pandas
 import yaml
 from aiohttp import web
 from deepdiff import DeepDiff
 from jinja2 import Environment, PackageLoader
-from report2sqaaas import utils as r2s_utils
-
-import openapi_server
 from openapi_server import config, controllers
 from openapi_server.controllers import crypto as crypto_utils
 from openapi_server.controllers import db
@@ -37,6 +35,7 @@ from openapi_server.controllers.git import GitUtils
 from openapi_server.controllers.jepl import JePLUtils
 from openapi_server.exception import SQAaaSAPIException
 from openapi_server.models.inline_object import InlineObject
+from report2sqaaas import utils as r2s_utils
 
 LEVELS_FOR_ASSESSMENT = ["REQUIRED", "RECOMMENDED"]
 
@@ -259,10 +258,10 @@ async def _get_tooling_for_assessment(
                                         os.path.relpath(_file, path)
                                         for _file in files_found
                                     ]
-                                    tool["args"] = (
-                                        ctls_utils.add_explicit_paths_for_tool(
-                                            tool["args"], _relative_paths
-                                        )
+                                    tool[
+                                        "args"
+                                    ] = ctls_utils.add_explicit_paths_for_tool(
+                                        tool["args"], _relative_paths
                                     )
                                 break
                         if not files_found:
@@ -742,6 +741,23 @@ async def add_pipeline_for_assessment(
 
     r = {"id": pipeline_id}
     return web.json_response(r, status=201)
+
+
+@ctls_utils.debug_request
+async def add_pipeline_for_assessment_custom(
+    request: web.Request, body, optional_tools=None
+) -> web.Response:
+    """Creates a pipeline for a custom assessment (QAA module).
+
+    Creates a pipeline for a custom assessment (QAA module).
+
+    :param body:
+    :type body: dict | bytes
+    :param optional_tools: Optional tools that shall be accounted
+    :type optional_tools: List[str]
+    """
+    body = ctls_utils.del_empty_keys(body)
+    return web.Response(status=200)
 
 
 @ctls_utils.debug_request
